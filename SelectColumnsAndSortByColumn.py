a5b97bab4097bae1ad1d84d6b5f362144529caef
Grace Molina <135928131+GraceMolina@users.noreply.github.com>
2026-05-12 19:10:08 -0400

    Add files via upload

--- SelectColumnsAndSortByColumn.py
+++ SelectColumnsAndSortByColumn.py
@@ -4,7 +4,6 @@ select columns and sort by an specific column
 """
 
 import csv
-import random
 
 def print_table(table):
     """
@@ -20,7 +19,7 @@ def read_csv_file(file_name):
     """
     table = []
     with open(file_name, newline = '') as csvfile:
-        csvreader = csv.reader(csvfile, skipinitialspace = True)
+        csvreader = csv.reader(csvfile, delimiter = ',')
         for row in csvreader:
             table.append(row)
         return table
@@ -31,7 +30,7 @@ def write_csv_file(csv_table, file_name):
     write the fields in the csv_table into a comma-separated CSV file with the name file_name.
     """
     with open(file_name, 'w', newline = '') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter = ',')
+        csvwriter = csv.writer(csvfile, delimiter = ',', quoting = csv.QUOTE_MINIMAL)
         for row in csv_table:
             csvwriter.writerow(row)
 
@@ -48,34 +47,49 @@ def sort_by_column(my_table, col_idx):
     Takes a list of lists named my_table and a column index named col_idx,
     returns a list of list sorted by the element indexed by col_idx.
     """
-    sorted_table = my_table.sort(key = lambda row: row[col_idx], reverse = True)
-    return sorted_table
+    my_table.sort(key = lambda row: float(row[col_idx]), reverse = True)
 
-# read a simple CSV file
-test_table = read_csv_file("test_case.csv")  # file is available in this repository
-print_table(test_table)
-print()
-
-# select the first and third column
-print_table(select_columns(test_table, [0, 2]))
-print()
-
-# sort by the fourth column
-sort_by_column(test_table, 3)
-print_table(test_table)
-print()
-
-# read cancer-risk data set, select columns A, B, C, E, and L, then sort by column E in descending order
-cancer_risk_table = read_csv_file("cancer_risk05_v4_county.csv")
-col_indices = [0, 1, 2, 4, 11]
-trimmed_risk_table = select_columns(cancer_risk_table, col_indices)
-sort_by_column(trimmed_risk_table, 4)
-
-# write trimmed and sorted cancer-risk data
-write_csv_file(trimmed_risk_table, "sorted_cancer_risk_trimmed.csv")
+def test_part2_code():
+    """
+    Run examples that test the functions for part 2
+    """
     
-# read the file "sorted_cancer_risk_trimmed.csv" and compare with your solution
-trimmed_risk_solution_copy = read_csv_file("sorted_cancer_risk_trimmed.csv")
+    # read a simple CSV file
+    test_table = read_csv_file("test_case.csv")
+    print_table(test_table)
+    print()
+    
+    # select the first and third column
+    print_table(select_columns(test_table, [0, 2]))
+    print()
+    
+    # sort by column 4
+    sort_by_column(test_table, 3)
+    print_table(test_table)
+    print()
 
-# test if sorted table and the copy are the same
-print(trimmed_risk_table == trimmed_risk_solution_copy)
+    # read cancer-risk data set, select columns A, B, C, E, and L, then sort by column E in descending order
+    cancer_risk_table = read_csv_file("cancer_risk05_v4_county.csv")
+    col_indices = [0, 1, 2, 4, 11]
+    trimmed_risk_table = select_columns(cancer_risk_table, col_indices)
+    sort_by_column(trimmed_risk_table, 4)
+    write_csv_file(trimmed_risk_table, "cancer_risk_trimmed.csv")
+    
+    # read the file "cancer_risk_trimmed_solution.csv" and compare with solution named trimmed_risk_table
+    trimmed_risk_solution = read_csv_file("cancer_risk_trimmed_solution.csv")
+    print(trimmed_risk_table == trimmed_risk_solution)
+
+test_part2_code()
+
+#Output from test_part2_code()
+##['1', '2', '3', '4']
+##['5', '6', '7', '8']
+##['-2', '-3', '-4', '-5']
+##
+##['1', '3']
+##['5', '7']
+##['-2', '-4']
+##
+##['5', '6', '7', '8']
+##['1', '2', '3', '4']
+##['-2', '-3', '-4', '-5']
