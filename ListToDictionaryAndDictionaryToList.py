9b07b82e629ca19e37fdc6598e154f73bd23bf7d
Grace Molina <135928131+GraceMolina@users.noreply.github.com>
2026-05-12 23:07:11 -0400

    Update ListToDictionaryAndDictionaryToList.py

--- ListToDictionaryAndDictionaryToList.py
+++ ListToDictionaryAndDictionaryToList.py
@@ -1,20 +1,20 @@
 """
-convert a list of tuples to a dictionary, and a dictionary to a list of tuples
+convert a list of 2-tuples to a dictionary, and a dictionary to a list of 2-tuples
 """
 
 def covert_list_to_dict(namelist):
     """
-    Takes a list of tuples and returns a dictionary. Each tuple is converted to an 
-    item in the dictionary, where the first component is the key and the second component is the corresponding value
+    Takes a list of 2-tuples and returns a dictionary. Each 2-tuple is converted to an 
+    item in the dictionary, where the first component is the key and the second component is the corresponding value.
     """
     dict = {pair[0] : pair[1] for pair in namelist}
     return dict
 
 def convert_dict_to_list(namedict):
     """
-    Takes a dictionary and returns a list of tuples. Each item in the 
-    dictionary is converted to a tuple, where the key is the first component 
-    and the value is the second component
+    Takes a dictionary and returns a list of 2-tuples. Each item in the 
+    dictionary is converted to a 2-tuple, where the key is the first component 
+    and the value is the second component.
     """
     list = [(key, value) for key, value in namedict.items()]
     return list
